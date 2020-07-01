a3ea1d7b57954eaa46c25b5a8acc38e99d10b4b7
Claudio Bartolini <cbartolini@box.com>
2020-06-30 23:16:59 -0700

    implemented logout flow

--- app/app-web/controllers/logoutController.js
+++ app/app-web/controllers/logoutController.js
@@ -2,9 +2,6 @@
 const config = require('config');
 
 module.exports.main = async (req, res, next) => {
-  if(req.query && req.query.accessToken) {
-    let boxAppUserId = req.user[BoxOptions.boxAppUserIdFieldName];
-    await Box.revokeUserToken(req.query.accessToken, boxAppUserId);
-  }
+  req.user = undefined;
   res.redirect('login');
 }
