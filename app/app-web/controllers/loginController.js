f03689be9a8db5f6d790022b2492b291d83a6aa3
Claudio Bartolini <cbartolini@box.com>
2020-06-30 23:08:49 -0700

    limited access to google user

--- app/app-web/controllers/loginController.js
+++ app/app-web/controllers/loginController.js
@@ -1,7 +1,11 @@
 'use strict';
 const config = require('config');
+const GoogleConfig = config.get('GoogleConfig');
 
 module.exports.callback = async (req, res, next) => {
 	console.log(req.user);
+	if (req.user.user_id !== GoogleConfig.googleUser) {
+		return res.redirect('/');
+	}
 	res.redirect('/user');
 }
