eab6a2c98a62ed7991323e5195c2843219e8afef
Claudio Bartolini <cbartolini@box.com>
2020-07-03 21:05:33 -0700

    corrected returnTo url when logging out Auth0

--- app/app-web/controllers/logoutController.js
+++ app/app-web/controllers/logoutController.js
@@ -7,11 +7,7 @@ const { Auth0Config } = require('../../config/default');
 module.exports.main = async (req, res, next) => {
   req.logout();
 
-  var returnTo = req.protocol + '://' + req.hostname;
-  var port = req.connection.localPort;
-  if (port !== undefined && port !== 80 && port !== 443) {
-    returnTo += ':' + port;
-  }
+  var returnTo = req.protocol + '://' + req.get('host');
   var logoutURL = new url.URL(
       'https://' + Auth0Config.domain + '/v2/logout'
     );
